45d3124770cd080a408e3b0dd26ae4b0e56982a4
faquanly <78623809+faquanly@users.noreply.github.com>
2023-11-28 10:37:00 -0800

    second commit

--- grade.sh
+++ grade.sh
@@ -6,11 +6,48 @@ rm -rf grading-area
 mkdir grading-area
 
 git clone $1 student-submission
-echo 'Finished cloning'
+echo 'Finished cloning...'
 
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+if [[ -f student-submission/ListExamples.java ]]
+then
+  echo 'ListExamples.java is found'
+else
+  echo 'ListExamples.java is not found'
+  echo 'Score: 0'
+fi
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+cp student-submission/ListExamples.java ./
+
+javac -cp $CPATH *.java
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+
+# The code will look like:
+
+# FAILURES!!!
+# Tests run: 4,  Failures: 2
+
+# We check for "FAILURES!!!" and do a bit of parsing of the last line to get the count
+
+FAIL=`grep -c FAILURES!!! junit-output.txt`
+
+if [[ $FAIL -eq 0 ]]
+then
+  echo "-----------------------"
+  echo "| All 4/4 tests passed |"
+  echo "-----------------------"
+else
+
+  RESULT_LINE=`grep "Tests run:" junit-output.txt`
+  COUNT=${RESULT_LINE:25:1}
+
+  echo "JUnit output was:"
+  cat junit-output.txt
+  echo ""
+  echo "-------------------"
+  echo "| Score: $COUNT/4 |"
+  echo "-------------------"
+  echo ""
+fi
